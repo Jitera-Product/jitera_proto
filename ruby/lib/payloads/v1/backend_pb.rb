00417e248f88b94508975c175163cc1ce61ce947
ak15 <atulkanswal@gmail.com>
2023-01-25 21:53:26 +0530

    Category

--- ruby/lib/payloads/v1/backend_pb.rb
+++ ruby/lib/payloads/v1/backend_pb.rb
@@ -229,6 +229,7 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
     end
     add_message "schema.v1.Controller.Response" do
       repeated :fields, :message, 1, "schema.v1.Controller.Response.Field"
+      optional :category, :string, 7
     end
     add_message "schema.v1.Controller.Response.Field" do
       optional :name, :string, 1
