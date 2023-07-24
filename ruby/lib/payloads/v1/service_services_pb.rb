01af1232c71b659607771acbe018c7b234bb29f4
eebasadre20 <eebasadre20@gmail.com>
2023-07-24 11:52:14 +0800

    added target env, event type and preview urls

--- ruby/lib/payloads/v1/service_services_pb.rb
+++ ruby/lib/payloads/v1/service_services_pb.rb
@@ -2,12 +2,10 @@
 # Source: v1/service.proto for package 'schema.v1'
 
 require 'grpc'
-
 module Schema
   module V1
     module CoreService
       class Service
-
         include ::GRPC::GenericService
 
         self.marshal_class_method = :encode
