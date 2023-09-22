c29b18b449331a52e78d45538376dba1ac43a5f1
MQuy <quy.truong@jitera.com>
2023-09-22 09:50:56 +0200

    feat(JAIS-175): add project token usage proto

--- php/src/Schema/V2/UserCaseCreationResponse.php
+++ php/src/Schema/V2/UserCaseCreationResponse.php
@@ -29,6 +29,10 @@ class UserCaseCreationResponse extends \Google\Protobuf\Internal\Message
      * Generated from protobuf field <code>.schema.v2.UserCaseCreationResponse.Status status = 4;</code>
      */
     protected $status = 0;
+    /**
+     * Generated from protobuf field <code>int32 token_usage = 5;</code>
+     */
+    protected $token_usage = 0;
 
     /**
      * Constructor.
@@ -40,6 +44,7 @@ class UserCaseCreationResponse extends \Google\Protobuf\Internal\Message
      *     @type string $message
      *     @type int $module
      *     @type int $status
+     *     @type int $token_usage
      * }
      */
     public function __construct($data = NULL) {
@@ -131,6 +136,28 @@ class UserCaseCreationResponse extends \Google\Protobuf\Internal\Message
     {
         GPBUtil::checkEnum($var, \Schema\V2\UserCaseCreationResponse\Status::class);
         $this->status = $var;
+
+        return $this;
+    }
+
+    /**
+     * Generated from protobuf field <code>int32 token_usage = 5;</code>
+     * @return int
+     */
+    public function getTokenUsage()
+    {
+        return $this->token_usage;
+    }
+
+    /**
+     * Generated from protobuf field <code>int32 token_usage = 5;</code>
+     * @param int $var
+     * @return $this
+     */
+    public function setTokenUsage($var)
+    {
+        GPBUtil::checkInt32($var);
+        $this->token_usage = $var;
 
         return $this;
     }
