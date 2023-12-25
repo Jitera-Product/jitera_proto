ef384dfba4ea62d2d45398efd5565df942c65f15
Kevin <comictvn@gmail.com>
2023-12-25 21:45:59 +0700

    feat/add-proto required project_preview file

--- ruby/lib/jitera_proto.rb
+++ ruby/lib/jitera_proto.rb
@@ -11,6 +11,7 @@ require_relative 'payloads/v2/user_case_creation_pb'
 require_relative 'payloads/v2/business_logic_pb'
 require_relative 'payloads/v2/block_pb'
 require_relative 'payloads/v2/project_figma_pb'
+require_relative 'payloads/v2/project_preview_pb'
 
 module JiteraProto
 end
