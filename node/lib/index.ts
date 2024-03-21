67372908a1dff54a9b38b378f3159199c15620e3
MQuy <quy.truong@jitera.com>
2024-03-21 13:30:12 +0700

    fix: export project git

--- node/lib/index.ts
+++ node/lib/index.ts
@@ -3,6 +3,7 @@ export * from "./protos/v1/project_import.proto";
 export * from "./protos/v1/project_export.proto";
 
 export * from "./protos/v2/project_source.proto";
+export * from "./protos/v2/project_git.proto";
 export * from "./protos/v2/specification.proto";
 export * from "./protos/v2/user_case_creation.proto";
 export * from "./protos/v2/business_logic.proto";
