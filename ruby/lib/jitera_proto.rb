4aba64db222ff78e0f6e102a24c78110c5626334
Kevin <comictvn@gmail.com>
2024-03-27 22:48:26 +0700

    JSR-719 fix require proto ticket

--- ruby/lib/jitera_proto.rb
+++ ruby/lib/jitera_proto.rb
@@ -16,7 +16,7 @@ require_relative 'payloads/v2/project_preview_pb'
 require_relative 'payloads/v2/project_git_pb'
 require_relative 'payloads/v2/translation_blocks_pb'
 require_relative 'payloads/v2/project_source_frontend_pb.rb'
-require_relative 'payloads/v2/project_ticket.rb'
+require_relative 'payloads/v2/project_ticket_pb.rb'
 
 module JiteraProto
 end
