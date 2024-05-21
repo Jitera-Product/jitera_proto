92ec3b7533e3007e87a9196b853c56f97bd2afac
Alex Klimenkkov <alex.klimenkovv@gmail.com>
2024-05-21 14:52:27 +0200

    feat: add actions to component specification

--- node/lib/protos/v2/component_specification.proto.ts
+++ node/lib/protos/v2/component_specification.proto.ts
@@ -1,26 +1,34 @@
 /* eslint-disable */
 import * as _m0 from "protobufjs/minimal";
 
-export class ComponentSpecification {
+export interface ComponentSpecification {
   type: string;
   title: string;
   description: string;
   figmaNodeId: string;
   boundingBox?: ComponentSpecificationBoundingBox;
+  actions: ComponentSpecificationAction[];
 }
 
-export class ComponentSpecificationBoundingBox {
+export interface ComponentSpecificationBoundingBox {
   top: number;
   left: number;
   right: number;
   bottom: number;
 }
 
-function createBaseComponentSpecification(): ComponentSpecification {
-  return { type: "", title: "", description: "", figmaNodeId: "" };
+export interface ComponentSpecificationAction {
+  type: string;
+  description: string;
+  reference: string;
+  referenceId: string;
 }
 
-export const ComponentSpecificationData = {
+function createBaseComponentSpecification(): ComponentSpecification {
+  return { type: "", title: "", description: "", figmaNodeId: "", actions: [] };
+}
+
+export const ComponentSpecification = {
   encode(message: ComponentSpecification, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
     if (message.type !== "") {
       writer.uint32(10).string(message.type);
@@ -35,7 +43,10 @@ export const ComponentSpecificationData = {
       writer.uint32(34).string(message.figmaNodeId);
     }
     if (message.boundingBox !== undefined) {
-      ComponentSpecificationBoundingBoxData.encode(message.boundingBox, writer.uint32(42).fork()).ldelim();
+      ComponentSpecificationBoundingBox.encode(message.boundingBox, writer.uint32(42).fork()).ldelim();
+    }
+    for (const v of message.actions) {
+      ComponentSpecificationAction.encode(v!, writer.uint32(50).fork()).ldelim();
     }
     return writer;
   },
@@ -60,7 +71,10 @@ export const ComponentSpecificationData = {
           message.figmaNodeId = reader.string();
           break;
         case 5:
-          message.boundingBox = ComponentSpecificationBoundingBoxData.decode(reader, reader.uint32());
+          message.boundingBox = ComponentSpecificationBoundingBox.decode(reader, reader.uint32());
+          break;
+        case 6:
+          message.actions.push(ComponentSpecificationAction.decode(reader, reader.uint32()));
           break;
         default:
           reader.skipType(tag & 7);
@@ -77,8 +91,11 @@ export const ComponentSpecificationData = {
       description: isSet(object.description) ? String(object.description) : "",
       figmaNodeId: isSet(object.figmaNodeId) ? String(object.figmaNodeId) : "",
       boundingBox: isSet(object.boundingBox)
-        ? ComponentSpecificationBoundingBoxData.fromJSON(object.boundingBox)
+        ? ComponentSpecificationBoundingBox.fromJSON(object.boundingBox)
         : undefined,
+      actions: Array.isArray(object?.actions)
+        ? object.actions.map((e: any) => ComponentSpecificationAction.fromJSON(e))
+        : [],
     };
   },
 
@@ -88,9 +105,15 @@ export const ComponentSpecificationData = {
     message.title !== undefined && (obj.title = message.title);
     message.description !== undefined && (obj.description = message.description);
     message.figmaNodeId !== undefined && (obj.figmaNodeId = message.figmaNodeId);
-    message.boundingBox !== undefined && (obj.boundingBox = message.boundingBox
-      ? ComponentSpecificationBoundingBoxData.toJSON(message.boundingBox)
-      : undefined);
+    message.boundingBox !== undefined &&
+      (obj.boundingBox = message.boundingBox
+        ? ComponentSpecificationBoundingBox.toJSON(message.boundingBox)
+        : undefined);
+    if (message.actions) {
+      obj.actions = message.actions.map((e) => e ? ComponentSpecificationAction.toJSON(e) : undefined);
+    } else {
+      obj.actions = [];
+    }
     return obj;
   },
 
@@ -101,8 +124,9 @@ export const ComponentSpecificationData = {
     message.description = object.description ?? "";
     message.figmaNodeId = object.figmaNodeId ?? "";
     message.boundingBox = (object.boundingBox !== undefined && object.boundingBox !== null)
-      ? ComponentSpecificationBoundingBoxData.fromPartial(object.boundingBox)
+      ? ComponentSpecificationBoundingBox.fromPartial(object.boundingBox)
       : undefined;
+    message.actions = object.actions?.map((e) => ComponentSpecificationAction.fromPartial(e)) || [];
     return message;
   },
 };
@@ -111,7 +135,7 @@ function createBaseComponentSpecificationBoundingBox(): ComponentSpecificationBo
   return { top: 0, left: 0, right: 0, bottom: 0 };
 }
 
-export const ComponentSpecificationBoundingBoxData = {
+export const ComponentSpecificationBoundingBox = {
   encode(message: ComponentSpecificationBoundingBox, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
     if (message.top !== 0) {
       writer.uint32(13).float(message.top);
@@ -179,6 +203,82 @@ export const ComponentSpecificationBoundingBoxData = {
     message.left = object.left ?? 0;
     message.right = object.right ?? 0;
     message.bottom = object.bottom ?? 0;
+    return message;
+  },
+};
+
+function createBaseComponentSpecificationAction(): ComponentSpecificationAction {
+  return { type: "", description: "", reference: "", referenceId: "" };
+}
+
+export const ComponentSpecificationAction = {
+  encode(message: ComponentSpecificationAction, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.type !== "") {
+      writer.uint32(10).string(message.type);
+    }
+    if (message.description !== "") {
+      writer.uint32(18).string(message.description);
+    }
+    if (message.reference !== "") {
+      writer.uint32(26).string(message.reference);
+    }
+    if (message.referenceId !== "") {
+      writer.uint32(34).string(message.referenceId);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): ComponentSpecificationAction {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseComponentSpecificationAction();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.type = reader.string();
+          break;
+        case 2:
+          message.description = reader.string();
+          break;
+        case 3:
+          message.reference = reader.string();
+          break;
+        case 4:
+          message.referenceId = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): ComponentSpecificationAction {
+    return {
+      type: isSet(object.type) ? String(object.type) : "",
+      description: isSet(object.description) ? String(object.description) : "",
+      reference: isSet(object.reference) ? String(object.reference) : "",
+      referenceId: isSet(object.referenceId) ? String(object.referenceId) : "",
+    };
+  },
+
+  toJSON(message: ComponentSpecificationAction): unknown {
+    const obj: any = {};
+    message.type !== undefined && (obj.type = message.type);
+    message.description !== undefined && (obj.description = message.description);
+    message.reference !== undefined && (obj.reference = message.reference);
+    message.referenceId !== undefined && (obj.referenceId = message.referenceId);
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<ComponentSpecificationAction>): ComponentSpecificationAction {
+    const message = createBaseComponentSpecificationAction();
+    message.type = object.type ?? "";
+    message.description = object.description ?? "";
+    message.reference = object.reference ?? "";
+    message.referenceId = object.referenceId ?? "";
     return message;
   },
 };
