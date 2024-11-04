f1020ae779b35d6eef89ad3bcd8f4c9cb8439c5a
MQuy <quy.truong@jitera.com>
2024-11-04 13:32:50 +0100

    feat: upgrade gprc

--- ruby/lib/payloads/v1/project_export_pb.rb
+++ ruby/lib/payloads/v1/project_export_pb.rb
@@ -8,28 +8,7 @@ require 'google/protobuf'
 descriptor_data = "\n\x17v1/project_export.proto\x12\tschema.v1\"\xec\x03\n\x06Report\x12\x14\n\x0cproject_name\x18\x01 \x01(\t\x12\x12\n\nproject_id\x18\x02 \x01(\x05\x12.\n\x08progress\x18\x03 \x01(\x0b\x32\x1a.schema.v1.Report.ProgressH\x00\x12.\n\x08\x63omplete\x18\x04 \x01(\x0b\x32\x1a.schema.v1.Report.CompleteH\x00\x12(\n\x05\x65rror\x18\x05 \x01(\x0b\x32\x17.schema.v1.Report.ErrorH\x00\x12\x39\n\x0eproject_export\x18\x06 \x01(\x0b\x32\x1f.schema.v1.Report.ProjectExportH\x01\x12;\n\x0fproject_preview\x18\x07 \x01(\x0b\x32 .schema.v1.Report.ProjectPreviewH\x01\x1a/\n\x08Progress\x12\x12\n\npercentage\x18\x01 \x01(\x02\x12\x0f\n\x07message\x18\x02 \x01(\t\x1a\x18\n\x08\x43omplete\x12\x0c\n\x04path\x18\x01 \x01(\t\x1a\x1b\n\rProjectExport\x12\n\n\x02id\x18\x01 \x01(\x05\x1a\x1c\n\x0eProjectPreview\x12\n\n\x02id\x18\x01 \x01(\x05\x1a\x18\n\x05\x45rror\x12\x0f\n\x07message\x18\x01 \x01(\tB\x07\n\x05stateB\r\n\x0breport_typeb\x06proto3"
 
 pool = Google::Protobuf::DescriptorPool.generated_pool
-
-begin
-  pool.add_serialized_file(descriptor_data)
-rescue TypeError => e
-  # Compatibility code: will be removed in the next major version.
-  require 'google/protobuf/descriptor_pb'
-  parsed = Google::Protobuf::FileDescriptorProto.decode(descriptor_data)
-  parsed.clear_dependency
-  serialized = parsed.class.encode(parsed)
-  file = pool.add_serialized_file(serialized)
-  warn "Warning: Protobuf detected an import path issue while loading generated file #{__FILE__}"
-  imports = [
-  ]
-  imports.each do |type_name, expected_filename|
-    import_file = pool.lookup(type_name).file_descriptor
-    if import_file.name != expected_filename
-      warn "- #{file.name} imports #{expected_filename}, but that import was loaded as #{import_file.name}"
-    end
-  end
-  warn "Each proto file must use a consistent fully-qualified name."
-  warn "This will become an error in the next major version."
-end
+pool.add_serialized_file(descriptor_data)
 
 module Schema
   module V1
