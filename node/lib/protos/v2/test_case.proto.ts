0f993a69f8a8e8fa4c412b3f735a081d76985f9a
Edward Jibson <edward.jibson@3sidedcube.com>
2024-11-07 16:53:28 +0000

    make optional

--- node/lib/protos/v2/test_case.proto.ts
+++ node/lib/protos/v2/test_case.proto.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import _m0 from "protobufjs/minimal";
+import * as _m0 from "protobufjs/minimal";
 import { Block, BlockData } from "./block_core.proto";
 import { BrowserStorageState, BrowserStorageStateData } from "./browser.proto";
 import {
