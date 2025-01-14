c9c1d7c8529e01375f8db46acb4e9503c99d58e2
eebasadre20 <eebasadre20@gmail.com>
2025-01-14 10:01:29 +0800

    bump package version

--- ruby/lib/payloads/v2/data_deletion_pb.rb
+++ ruby/lib/payloads/v2/data_deletion_pb.rb
@@ -5,7 +5,7 @@
 require 'google/protobuf'
 
 
-descriptor_data = "\n\x16v2/data_deletion.proto\x12\tschema.v2\"C\n\x13TeamDeletionRequest\x12,\n\x0cproject_data\x18\x01 \x03(\x0b\x32\x16.schema.v2.ProjectData\">\n\x0bProjectData\x12\x12\n\nproject_id\x18\x01 \x01(\x05\x12\x1b\n\x13project_generate_id\x18\x02 \x03(\x05\x62\x06proto3"
+descriptor_data = "\n\x16v2/data_deletion.proto\x12\tschema.v2\"J\n\x13TeamDeletionRequest\x12\x33\n\x0cproject_data\x18\x01 \x03(\x0b\x32\x1d.schema.v2.ProjectDeletedData\"E\n\x12ProjectDeletedData\x12\x12\n\nproject_id\x18\x01 \x01(\x05\x12\x1b\n\x13project_generate_id\x18\x02 \x03(\x05\x62\x06proto3"
 
 pool = Google::Protobuf::DescriptorPool.generated_pool
 pool.add_serialized_file(descriptor_data)
@@ -13,6 +13,6 @@ pool.add_serialized_file(descriptor_data)
 module Schema
   module V2
     TeamDeletionRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("schema.v2.TeamDeletionRequest").msgclass
-    ProjectData = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("schema.v2.ProjectData").msgclass
+    ProjectDeletedData = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("schema.v2.ProjectDeletedData").msgclass
   end
 end
