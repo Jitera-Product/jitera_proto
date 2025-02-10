1b40a60457a844884106176f29dfc797b383fbfe
ak15 <atulkanswal@gmail.com>
2025-02-10 12:09:38 +0530

    Fix

--- node/lib/protos/v2/project_generate_queue.proto.ts
+++ node/lib/protos/v2/project_generate_queue.proto.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import * as _m0 from "protobufjs/minimal";
+import Long from "long";
+import _m0 from "protobufjs/minimal";
 
 export class ProjectGenerateReport {
   projectGenerateQueueId: number;
@@ -14,6 +15,13 @@ export class ProjectGenerateReportProgress {
 
 export class ProjectGenerateReportError {
   message: string;
+}
+
+export class ProjectGenerateProgress {
+  projectGenerateQueueId: number;
+  statusKey: string;
+  /** Unix time in seconds */
+  eventTimestamp: number;
 }
 
 function createBaseProjectGenerateReport(): ProjectGenerateReport {
@@ -193,12 +201,111 @@ export const ProjectGenerateReportErrorData = {
   },
 };
 
+function createBaseProjectGenerateProgress(): ProjectGenerateProgress {
+  return { projectGenerateQueueId: 0, statusKey: "", eventTimestamp: 0 };
+}
+
+export const ProjectGenerateProgressData = {
+  encode(message: ProjectGenerateProgress, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.projectGenerateQueueId !== 0) {
+      writer.uint32(8).int32(message.projectGenerateQueueId);
+    }
+    if (message.statusKey !== "") {
+      writer.uint32(18).string(message.statusKey);
+    }
+    if (message.eventTimestamp !== 0) {
+      writer.uint32(24).int64(message.eventTimestamp);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): ProjectGenerateProgress {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseProjectGenerateProgress();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.projectGenerateQueueId = reader.int32();
+          break;
+        case 2:
+          message.statusKey = reader.string();
+          break;
+        case 3:
+          message.eventTimestamp = longToNumber(reader.int64() as Long);
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): ProjectGenerateProgress {
+    return {
+      projectGenerateQueueId: isSet(object.projectGenerateQueueId) ? Number(object.projectGenerateQueueId) : 0,
+      statusKey: isSet(object.statusKey) ? String(object.statusKey) : "",
+      eventTimestamp: isSet(object.eventTimestamp) ? Number(object.eventTimestamp) : 0,
+    };
+  },
+
+  toJSON(message: ProjectGenerateProgress): unknown {
+    const obj: any = {};
+    message.projectGenerateQueueId !== undefined &&
+      (obj.projectGenerateQueueId = Math.round(message.projectGenerateQueueId));
+    message.statusKey !== undefined && (obj.statusKey = message.statusKey);
+    message.eventTimestamp !== undefined && (obj.eventTimestamp = Math.round(message.eventTimestamp));
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<ProjectGenerateProgress>): ProjectGenerateProgress {
+    const message = createBaseProjectGenerateProgress();
+    message.projectGenerateQueueId = object.projectGenerateQueueId ?? 0;
+    message.statusKey = object.statusKey ?? "";
+    message.eventTimestamp = object.eventTimestamp ?? 0;
+    return message;
+  },
+};
+
+declare var self: any | undefined;
+declare var window: any | undefined;
+declare var global: any | undefined;
+var tsProtoGlobalThis: any = (() => {
+  if (typeof globalThis !== "undefined") {
+    return globalThis;
+  }
+  if (typeof self !== "undefined") {
+    return self;
+  }
+  if (typeof window !== "undefined") {
+    return window;
+  }
+  if (typeof global !== "undefined") {
+    return global;
+  }
+  throw "Unable to locate global object";
+})();
+
 type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
 
 type DeepPartial<T> = T extends Builtin ? T
   : T extends Array<infer U> ? Array<DeepPartial<U>> : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
   : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
   : Partial<T>;
+
+function longToNumber(long: Long): number {
+  if (long.gt(Number.MAX_SAFE_INTEGER)) {
+    throw new tsProtoGlobalThis.Error("Value is larger than Number.MAX_SAFE_INTEGER");
+  }
+  return long.toNumber();
+}
+
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any;
+  _m0.configure();
+}
 
 function isSet(value: any): boolean {
   return value !== null && value !== undefined;
