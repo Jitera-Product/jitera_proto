7aeda73ec5e2acd0ed93b042eced64172679200d
Alex K <alex.klimenkovv@gmail.com>
2025-02-12 18:26:33 +0100

    remove unused Value

--- node/lib/protos/v2/generate_doc.proto.ts
+++ node/lib/protos/v2/generate_doc.proto.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import * as _m0 from "protobufjs/minimal";
-import { Value, ValueData } from "../google/protobuf/struct.proto";
+import { ValueData } from "../google/protobuf/struct.proto";
 
 export class GenerateDocResponse {
   projectGenerateQueueId: number;
